90a4f87a2cbfbfd5997bdee20de0f4d96813613e
zephyraoh <marwannarockyou@hotmail.fr>
2022-08-24 13:36:14 +0200

    account profile picture feature

--- Front/Obook/src/components/PageSpecificComponents/Account.jsx
+++ Front/Obook/src/components/PageSpecificComponents/Account.jsx
@@ -1,16 +1,37 @@
 import { useDispatch } from "react-redux";
 import { SetUserLabel } from "../../actions/user";
+import Axios from "axios";
+import { useState } from "react";
+
 
 const Account = () =>{
     const dispatch = useDispatch();
-    
-    const handleClick = (e) => {
-    console.log(`button ${e.target.value} clicked`);
-    dispatch(SetUserLabel(e.target.value))
-}
+    const [imageSelected, setImageSelected] = useState("")
+    const uploadImage = (e)=>{
 
+        //A dispatch dans une action middleware (auth?)
+        const selectedFiles = e.target.files;
+        console.log("last selected file ==>", selectedFiles[0]);
+        const formData = newFormData()
+        formData.append("file", imageSelected)
+        formData.append("upload_preset", "visitor of Obook website")
+        console.log("our final formData ==>", formData)
+        
+        Axios.post("https://api.cloudinary.com/v1_1/obook/upload",formData)
+        .then((response)=>{
+            console.log(response)
+        })
+    }
+
+    const handleClick = (e) => {
+        console.log(`button ${e.target.value} clicked`);
+        dispatch(SetUserLabel(e.target.value))
+
+}
     return (
     <>
+    <input type="file" onChange={event=>setImageSelected(event.target.files[0])}/>
+    <button name="upload Image" onClick={uploadImage}>Upload Image</button>
         <button value ="Romans" name="profileLabelButton" onClick={handleClick}>Romans</button>
         <button value = 'Science-Fiction & Fantasy' name="profileLabelButton" onClick={handleClick}>Science-Fiction & Fantasy</button>
         <button value ="Polar & Thriller" name="profileLabelButton" onClick={handleClick}>Polar & Thriller</button>
