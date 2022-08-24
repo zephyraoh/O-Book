ea8e66e782b0410f26595f3710580212b20da96f
zephyraoh <marwannarockyou@hotmail.fr>
2022-08-24 11:29:31 +0200

    persistStore added

--- Front/Obook/src/main.jsx
+++ Front/Obook/src/main.jsx
@@ -4,15 +4,18 @@ import App from './components/App/App';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import store, {Persistor} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={Persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- Front/Obook/src/store/index.js
+++ Front/Obook/src/store/index.js
@@ -3,6 +3,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducers';
 import authMiddleware from '../middlewares/auth';
 import searchMiddleware from '../middlewares/search'; 
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage '
+
+const persistConfig={
+  key:'main-root',
+  storage,
+}
+
+const persistedReducer=persistReducer(persistConfig, reducer)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -10,6 +19,8 @@ const enhancers = composeEnhancers(
     applyMiddleware(authMiddleware, searchMiddleware),
   );
   
-  const store = createStore(reducer, enhancers);
-  
+  const store = createStore(persistedReducer, enhancers);
+  const Persistor= persistStore(store);
+
+  export {Persistor}
   export default store;
