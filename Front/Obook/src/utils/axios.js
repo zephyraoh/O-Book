3c9c62dbd4c46893b12259c42cc339f71d097b97
Duke Padmore <dukepadmore@macbook-air-de-duke.home>
2022-09-01 13:55:15 +0200

    Back deployed on Heroku

--- Front/Obook/src/utils/axios.js
+++ Front/Obook/src/utils/axios.js
@@ -2,13 +2,6 @@ import axs from 'axios';
 
 const apiKey = import.meta.env.VITE_ISBN_API_KEY;
 
-export const axiosBooksApi = axs.create({
-	baseURL: 'https://www.googleapis.com/books/v1/volumes?q=',
-	
-});
-
-// Recherche fonctionnelle : pour une recherche classique, ajouter la search value à l'URL + un point d'interrogation.
-// Pour ajouter un filtre par titre ou par auteur, ajouter &column=title ou &column=author à la fin de l'url
 export const ISBNApiSearchBar = axs.create({
 	baseURL: 'https://api2.isbndb.com',
 	headers: {'Authorization': apiKey},
@@ -16,9 +9,9 @@ export const ISBNApiSearchBar = axs.create({
 });
 
 export const axiosServerDB = axs.create({
-	baseURL: 'http://localhost:5999',//URL DU FUTUR SERVEUR BACK O'BOOK
+	baseURL: 'https://obook-api.herokuapp.com/api',
 	// POUR LA PARTIE CORS, AJOUTER :
-	// withCredentials: true,
+	withCredentials: true,
 });
 
 
